61624a1121926fa8332547257402890e2934bf30
Yan <yans@yancomm.net>
2024-08-09 10:57:18 -0700

    launch memory level

--- memory/mem-load/.py/chal.py
+++ memory/mem-load/.py/chal.py
@@ -7,23 +7,24 @@ import re
 allow_asm = True
 give_flag = True
 returncode = None
-secret_value = random.choice(list(set(range(15, 250))-{100}))
+secret_value = random.randint(15, 255)
+secret_addr = 133700
 assembly_prefix = f"""
     mov r9, 0x0
     mov r8, 0xffffffff
     mov r10, 0x32
     mov rdx, 0x3
     mov rsi, 0x1000
-    mov rdi, 0
+    mov rdi, 131072
     mov rax, 9
     syscall
-	mov byte ptr [100], {secret_value}
+	mov byte ptr [{secret_addr}], {secret_value}
 """
 num_instructions = 3
 
 check_runtime_prologue = """
 \033[92mLet's check what your exit code is! It should be our secret
-value stored at memory address 100 (value {secret_value}) to succeed!
+value stored at memory address {secret_addr} (value {secret_value}) to succeed!
 
 Go go go!
 \033[0m
@@ -37,43 +38,43 @@ performed your first memory read. Great job!
 \033[0m
 """.strip()
 
-check_runtime_failure = """
+check_runtime_failure = f"""
 \033[0;31m
 Your program exited with the wrong error code. Please make sure
-to move the memory at address 100 into 'dil'.
+to move the memory at address {secret_addr} into 'rdi'.
 """.strip()
 
 def check_disassembly(disas):
 	assert disas[0].mnemonic == "mov" and disas[1].mnemonic == "mov", (
 		"Your first two instructions must be 'mov' instructions: one to\n"
-		"move a value from memory into dil, and one to move a value into al.\n"
+		"move a value from memory into rdi, and one to move a value into rax.\n"
 	)
 
 	opnds1 = disas[0].op_str.split(", ")
 	opnds2 = disas[1].op_str.split(", ")
 	regs, _ = zip(opnds1, opnds2)
-	assert set(regs) == { 'al', 'dil' }, (
-		"You must set both the al register and the dil register!"
+	assert set(regs) == { 'rax', 'rdi' }, (
+		"You must set both the rax register and the rdi register!"
 	)
 
-	assert ( ['al','0x3c'] in [ opnds1, opnds2 ] ), (
-		"You must properly set the 'exit' system call number (60 in al)!"
+	assert ( ['rax','0x3c'] in [ opnds1, opnds2 ] ), (
+		"You must properly set the 'exit' system call number (60 in rax)!"
 	)
 
-	dil_opnd = opnds1[1] if opnds2[0] == 'al' else opnds2[1]
+	rdi_opnd = opnds1[1] if opnds2[0] == 'rax' else opnds2[1]
 
-	assert dil_opnd != "0x64", (
-		"You are moving the value 100 into dil, not the data stored at the memory\n"
-		"addressed by the address 100! Please use the [ADDRESS] syntax to denote\n"
-		"the actual memory address (in this case, ADDRESS should be 100)."
+	assert rdi_opnd != hex(secret_addr), (
+		f"You are moving the value {secret_addr} into rdi, not the data stored at the memory\n"
+		f"addressed by the address {secret_addr}! Please use the [ADDRESS] syntax to denote\n"
+		f"the actual memory address (in this case, ADDRESS should be {secret_addr})."
 	)
-	assert dil_opnd.startswith("byte ptr ["), (
-		"You are not moving a value from memory to dil. You must use the '[ADDRESS]'\n"
+	assert rdi_opnd.startswith("qword ptr ["), (
+		"You are not moving a value from memory to rdi. You must use the '[ADDRESS]'\n"
 		"syntax to do this. In this case, I've stored the secret value at the\n"
-		"ADDRESS of 100."
+		f"ADDRESS of {secret_addr}."
 	)
-	assert re.match(r"byte ptr \[\w+\]", dil_opnd), (
-		"In this level, please use the address 100 directly for the memory address.\n"
+	assert re.match(r"qword ptr \[\w+\]", rdi_opnd), (
+		f"In this level, please use the address {secret_addr} directly for the memory address.\n"
 		"We will learn more advanced ways of addressing memory later."
 	)
 
--- memory/mem-load/DESCRIPTION.md
+++ memory/mem-load/DESCRIPTION.md
@@ -1,6 +1,6 @@
 As seen by your program, computer memory is a huge place where data is housed.
 Like houses on a street, every part of memory has a numeric _address_, and like houses on a street, these numbers are (mostly) sequential.
-Modern computers have enormous amounts of memory, and the view of memory of a typical modern program actually has large gaps (think: a playground that takes up some amount of house numbers, but no one lives there!).
+Modern computers have enormous amounts of memory, and the view of memory of a typical modern program actually has large gaps (think: a portion of the street that hasn't had houses built on it, and so those addresses are skipped).
 But these are all details: the point is, computers store data, mostly sequentially, in memory.
 
 In this level, we will practice accessing data stored in memory.
@@ -8,26 +8,26 @@ How might we do this?
 Recall that to move a value into a register, we did something like:
 
 ```assembly
-mov dil, 42
+mov rdi, 31337
 ```
 
-After this, the value of `dil` is `42`.
+After this, the value of `rdi` is `31337`.
 Cool.
 Well, we can use the same instruction to access memory!
 There is another format of the command that, instead, uses the second parameter as an address to access memory!
 It is:
 
 ```assembly
-mov dil, [42]
+mov rdi, [31337]
 ```
 
-When the CPU executes this instruction, it of course understands that the `42` is an _address_, not a raw value.
-If you think of the instruction as a person telling the CPU what to do, and we stick with our "houses on a street" analogie, then instead of just handing the CPU data, the instruction/person _points at a house on the street_.
-The CPU will then go to that address, ring its doorbell, open its front door, drag the data that's in there out, and put it into `dil`.
-Thus, the `42` in this context is the _memory address_ and serves as a _pointer_ to the data stored at that memory address.
+When the CPU executes this instruction, it of course understands that `31337` is an _address_, not a raw value.
+If you think of the instruction as a person telling the CPU what to do, and we stick with our "houses on a street" analogy, then instead of just handing the CPU data, the instruction/person _points at a house on the street_.
+The CPU will then go to that address, ring its doorbell, open its front door, drag the data that's in there out, and put it into `rdi`.
+Thus, the `31338` in this context is the _memory address_ and serves to _point to_ the data stored at that memory address.
 
 Let's put this into practice!
-I've stored a secret number at memory address `100`.
+I've stored a secret number at memory address `133700`.
 You must retrieve this secret number and use it as the exit code for your program.
-To do this, you must read it into `dil`, whose value, if you recall, is the first parameter to `exit` and is used as the exit code.
+To do this, you must read it into `rdi`, whose value, if you recall, is the first parameter to `exit` and is used as the exit code.
 Good luck!
