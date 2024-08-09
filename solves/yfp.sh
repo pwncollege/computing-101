46e7db47a8b0b5ce78ce676682c3ba64fd5fc6e0
Yan <yans@yancomm.net>
2024-08-09 10:41:50 -0700

    wtf

--- solves/yfp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-cd $(dirname ${BASH_SOURCE[0]})/../
-
-your-first-program/rax/check <<< "mov rax, 60" | grep "pwn.college{"
-your-first-program/exit/check <<< "mov rax, 60; syscall" | grep "pwn.college{"
-your-first-program/exit-code/check <<< "mov rdi, 42; mov rax, 60; syscall" | grep "pwn.college{"
